189cc8948de5b1a04adf0f823778d50be4b5eea6
tanka <>
2023-10-18 18:21:20 +0800

    feat: 增加prettier配置

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: tanka 
+ * @Date: 2023-10-18 18:12:36
+ * @LastEditors: tanka 
+ * @LastEditTime: 2023-10-18 18:12:54
+ * @FilePath: /common/.prettierrc.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const fabric = require('@umijs/fabric');
+
+module.exports = {
+  ...fabric.prettier,
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 /*
- * @Author: tanka 
+ * @Author: tanka
  * @Date: 2023-06-16 15:10:27
- * @LastEditors: tanka 
- * @LastEditTime: 2023-10-18 10:44:28
+ * @LastEditors: tanka
+ * @LastEditTime: 2023-10-18 18:17:53
  * @FilePath: /common/src/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+
 const test = () => {
   const a = 123;
+
   console.log(a);
 };
 
